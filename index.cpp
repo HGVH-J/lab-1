525d0353b8cb0eed5f1d4d16e7f1cd27147cfe99
HGVH-J <stratiynazar008@gmail.com>
2025-02-26 10:03:34 +0200

    My first commit

---
+++ index.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int main() {
+	setlocale(LC_ALL, "RU");
+	std::cout << "no problem" <<std::endl;
+	std::cout << "aegis nortos"<<  std::endl;
+	std::cout << "прототип";
+	return 0;
+}
